115951a7edccda3767be91430ed950394dcd6f81
莫载斌 <zaibin_mo@sui.com>
2020-07-15 16:31:36 +0800

    add 群报数构造表单数据 api

--- ProductApi/MiniProgramForm/api.py
+++ ProductApi/MiniProgramForm/api.py
@@ -4,27 +4,42 @@
 # @Author : mocobk
 # @Email  : mailmzb@qq.com
 # @Time   : 2019/1/22 9:53
+import mimetypes
+
+import requests
+
 from ProductApi.MiniProgramForm import config
 from ProductApi.base import ApiBase, Response
 
 
 class FormApi(ApiBase):
 
-    def __init__(self, fuid, print_results=False):
+    def __init__(self, fuid=None, print_results=False):
         """
-        :param fuid: 用户群报数 id
+        :param fuid: 用户群报数 id, 不传则使用配置中默认的
         """
-        self.config: config.Test = getattr(config, self.env.name)
-        self.fuid = fuid
-        self.authorized_hearders = self.get_authorized_hearders()
         super().__init__(print_results)
+        self.config: config.Test = getattr(config, self.env.name)
+        self.fuid = fuid or self.config.DUFAULT_FUID
+        self.authorized_hearders = self.get_authorized_hearders()
 
     def get_authorized_hearders(self) -> dict:
         """
         TODO: 待实现, 登录失败直接抛出异常
         """
-        # self.fuid
-        return {'Authorization': '330ebabccd6b19ad3738776f50a5081944e1dfcfa6698fd2caa04585a07a994b'}
+        url = self.config.HOSTNAME + self.config.Url.v1_login_test
+        data = {
+            "appId": self.config.APP_ID,
+            "fuid": self.fuid
+        }
+        self.set_logger_off()
+        self.logger.info('正在登陆...')
+        response = super().request(url=url, method='POST', json=data)
+        self.set_logger_on()
+
+        if response.status_code != 200:
+            raise Exception('登陆失败！请检查环境和账号')
+        return {'Authorization': response.data.get('data', {}).get('token', '')}
 
     def request(self, method, url,
                 params=None, data=None, json=None, headers: dict = None, cookies=None, files=None,
@@ -34,6 +49,11 @@ class FormApi(ApiBase):
         need_auth:  是否需要鉴权， 服务端这边对于不需要鉴权的但传
         :return:
         """
+        if isinstance(url, str):
+            url = self.config.HOSTNAME + url
+        elif isinstance(url, (tuple, list)):
+            url = ''.join(url)
+
         headers = {**headers, **self.authorized_hearders} if headers else self.authorized_hearders
         response = super().request(method, url, params, data, json, headers, cookies, files, auth, timeout,
                                    allow_redirects, proxies, hooks, stream, verify, cert)
@@ -61,15 +81,40 @@ class FormApi(ApiBase):
         """
         Name: 我创建的表单列表
         """
-        self.authorized_hearders = {}
         url = self.config.Url.v1_examples
         response = self.request(url=url, method='GET')
+        return response
+
+    def v1_image(self, image: str):
+        """
+        Name: 上传图片
+        file: 可以是本地文件路径，也可以是 url
+        """
+        url = self.config.Url.v1_image
+        if image.startswith('http'):
+            res = requests.get(image, stream=True)
+            fp = res.content
+            name = 'image.jpg'
+            content_type = res.headers.get('Content-Type', 'image/jpeg')
+        else:
+            fp = open(image, 'rb')
+            name = os.path.basename(image)
+            content_type = mimetypes.guess_type(image)[0]
+
+        files = {'file': (name, fp, content_type)}
+        response = self.request(url=url, method='POST', files=files)
+        return response
+
+    def v1_form(self, data):
+        url = self.config.Url.v1_form
+        response = self.request(url=url, method='POST', json=data)
         return response
 
 
 if __name__ == '__main__':
     import os
-    os.environ['env'] = 'uat'
-    api = FormApi(fuid='123', print_results=True)
-    # api.v1_creation_forms(params={'pageNo': 1, 'pageSize': 50})
+
+    os.environ['env'] = 'production'
+    api = FormApi(fuid='1026957780256297009', print_results=True)
+    api.v1_creation_forms(params={'pageNo': 1, 'pageSize': 50})
     api.v1_examples()
---
+++ ProductApi/MiniProgramForm/form/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File   : __init__.py.py
+# @Author : mocobk
+# @Email  : mailmzb@qq.com
+# @Time   : 2020/7/15 14:58
+from .form import CreateActivityForm, CreateShoppingForm
+
+__all__ = [
+    'CreateShoppingForm',
+    'CreateActivityForm'
+]
