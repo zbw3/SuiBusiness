3ace51bbc5a32886a832b5ab2596b8c336c592df
莫载斌 <zaibin_mo@sui.com>
2020-08-12 10:42:40 +0800

    update 构造数据用例

--- test_cases/mp_form/fake_form.py
+++ test_cases/mp_form/fake_form.py
@@ -7,10 +7,12 @@
 """
 该用例主要是为了快速构造旧版本的表单数据，以便观察是否有数据兼容问题出现
 """
+import random
+
 import pytest
 
 from ProductApi.MiniProgramForm.form.enum import FormStatus
-from test_cases.mp_form import verify_post_form
+from test_cases.mp_form import verify_post_form, create_form, create_form_data
 
 
 def test_create_normal_activity_form(user1, default_activity_form):
@@ -54,20 +56,20 @@ def test_create_finished_activity_form(user1, default_activity_form):
 
 
 def test_create_normal_shopping_form(user1, default_shopping_form):
-    """创建正常进行中的 [活动接龙] 表单"""
+    """创建正常进行中的 [商品接龙] 表单"""
     default_shopping_form.set_title('进行中-含填写项')
     verify_post_form(user1, default_shopping_form)
 
 
 def test_create_normal_with_no_question_shopping_form(user1, default_shopping_form):
-    """创建正常进行中的 [活动接龙] 表单"""
+    """创建正常进行中的 [商品接龙] 表单"""
     default_shopping_form.set_title('进行中-无填写项')
     default_shopping_form.clear_questions()
     verify_post_form(user1, default_shopping_form)
 
 
 def test_create_paused_shopping_form(user1, default_shopping_form):
-    """创建停止中的 [活动接龙] 表单"""
+    """创建停止中的 [商品接龙] 表单"""
     default_shopping_form.set_title('停止中-含填写项')
     form_id = verify_post_form(user1, default_shopping_form)
     update_status_res = user1.v1_form_id_status(form_id, FormStatus.PAUSED.value)
@@ -75,17 +77,29 @@ def test_create_paused_shopping_form(user1, default_shopping_form):
 
 
 def test_create_unopened_shopping_form(user1, default_shopping_form):
-    """创建未开启的 [活动接龙] 表单"""
+    """创建未开启的 [商品接龙] 表单"""
     default_shopping_form.set_title('未开启-含填写项')
     default_shopping_form.set_duration_time(start=default_shopping_form.now_offset(days=1))
     verify_post_form(user1, default_shopping_form)
 
 
 def test_create_finished_shopping_form(user1, default_shopping_form):
-    """创建已结束的 [活动接龙] 表单"""
+    """创建已结束的 [商品接龙] 表单"""
     default_shopping_form.set_title('已结束-含填写项')
     default_shopping_form.set_duration_time(end=default_shopping_form.now_offset(seconds=1))
     verify_post_form(user1, default_shopping_form)
+
+
+"""
+=================================================================================================
+"""
+
+def test_create_many_order_shopping_form(user1, user2, default_shopping_form):
+    """创建有多个接龙的 [商品接龙] 表单"""
+    default_shopping_form.set_title('含40个接龙数据')
+    form_id = verify_post_form(user1, default_shopping_form)
+    for i in range(40):
+        create_form_data(random.choice([user1, user2]), form_id)
 
 
 if __name__ == '__main__':
